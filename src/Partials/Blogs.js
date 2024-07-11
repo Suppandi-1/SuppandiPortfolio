d9611ff2c5d198823393867db44d9c41d6a6b9f0
Devesh <deveshdaku@gmail.com>
2024-07-12 03:39:38 +0530

    Blog page added

--- src/Partials/Blogs.js
+++ src/Partials/Blogs.js
@@ -1,7 +1,19 @@
+import BlogsData from "./BlogsData";
+import LinkData from "./LinkData";
+
 const Blogs = () => {
     return ( 
-        <div>
-            hello suppandi
+        <div className="  roboto text-shadow justify-around items-center   pt-32  flex  w-full h-full ">
+            <section className="  h-full w-auto flex flex-col justify-around">
+                <h2 className=" text-4xl md:text-5xl text-p2 font-bold text-center ">My <span className="text-p5"> Blogs </span></h2>
+                
+                    <BlogsData />
+             
+            </section>
+            {/* <section className="h-full w-auto">
+                <h2 className=" text-4xl md:text-5xl text-p2 font-bold ">My <span className="text-p5"> Links </span></h2>
+                <LinkData/>
+            </section> */}
         </div>
      );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,25 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'transparent': 'transparent',
+        'current': 'currentColor',
+        'white': '#ffffff',
+        'purple': '#3f3cbb',
+        'midnight': '#121063',
+        'metal': '#565584',
+        'tahiti': '#3ab7bf',
+        'silver': '#ecebff',
+        'bubble-gum': '#ff77e9',
+        'bermuda': '#78dcca',
+        'neone': '#39ff14',
+        'p1': '#F4F1DE',
+        'p2': '#E07A5F',
+        'p3': '#3D405B',
+        'p4': '#81B29A',
+        'p5': '#F2CC8F',
 
-      screens: {
-        'tall': { 'raw': '(min-height: 800px)' },
-        // => @media (min-height: 800px) { ... }
-      }
+      },
     },
   },
   plugins: [],
